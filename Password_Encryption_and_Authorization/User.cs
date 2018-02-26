4cd2ef44c36940cbb95fd7b75f7e73ed58d408f7
Greg Wilson <greg.wilson5@outlook.com>
2018-02-26 12:31:21 -0500

    Adds class UI and struct User

---
+++ Password_Encryption_and_Authorization/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Encryption_and_Authorization
+{
+    struct User
+    {
+        public CurrentUser(string UserName, string password)
+        {
+
+        }
+
+    }
+}
