b76e144d47c72442cb026d6c0d7ed5621dc660a2
Greg Wilson <greg.wilson5@outlook.com>
2018-03-04 22:15:06 -0500

    implements all code from whiteboarding and appropriate classes.

--- Password_Encryption_and_Authorization/Program.cs
+++ Password_Encryption_and_Authorization/Program.cs
@@ -8,13 +8,6 @@ namespace Password_Encryption_and_Authorization
 {
     class Program
     {
-
-        //These should probably be in a struct and not a list - the
-        //user name and passwords should be together and easily searchable
-        //as a pair...
-        public static List<string> inputUserName = new List<string>();
-        public static List<string> inputPassword = new List<string>();
-
         static void Main(string[] args)
         {
             var input = false;
@@ -22,7 +15,7 @@ namespace Password_Encryption_and_Authorization
             {
                 try
                 {
-                    PasswordAuthentication();
+                    new UserInterface().PasswordAuthentication();
                     input = true;
                 }
                 catch
@@ -30,167 +23,6 @@ namespace Password_Encryption_and_Authorization
                     Console.WriteLine("Error - please try again... ");
                 }
             } while (!input);
-        }
-
-        //Main Menu - calls UserInput and acts on item methods
-        private static void PasswordAuthentication()
-        {
-            do
-            {
-                Console.Clear();
-                Console.WriteLine("\nPASSWORD AUTHENTICATION SYSTEM\n");
-                Console.WriteLine("-----------------------------------------\n");
-                Console.WriteLine("Please select one option:\n");
-                Console.WriteLine("1. Establish an account");
-                Console.WriteLine("2. Authenticate a user");
-                Console.WriteLine("3. Exit the system\n");
-            }
-            while (!(ActOnSelectedItem(UserInput())));
-        }
-
-        //Obtains User Input and returns selection 
-        public static int UserInput()
-        {
-            var input = false;
-
-            int selection = 0;
-
-            do
-            {
-                Console.Write(" Make a selection :");
-
-                try
-                {
-                    selection = int.Parse(Console.ReadLine());
-                    if (selection < 4 && selection >0)
-                    {
-                        input = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Enter selection between 1 and 3. Try again.\n");
-                    }
-                    
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Invalid selection, Try again!");
-                }
-            } while (!input);
-
-            return selection;
-
-        }
-
-        //switch case to determine which method to call based on selection
-        private static bool ActOnSelectedItem(int selection)
-        {
-            var exit = false;
-
-            switch (selection)
-            {
-                case 1:
-                    EstablishUser();
-                    break;
-                case 2:
-                    AuthenticateUser();
-                    break;
-                case 3:
-                    exit = true;
-                    break;
-                default:
-                    exit = true;
-                    break;
-            }
-            return exit;
-        }
-
-        //received user name and password to determine if they match
-        private static void AuthenticateUser()
-        {
-            int attempsAtLogin = 0;
-
-            for (int i = 0; i<3; i++)
-            {
-                Console.Clear();
-                Console.WriteLine("\nPASSWORD AUTHENTICATION SYSTEM\n");
-                Console.WriteLine("Authenticate User and Password\n");
-                Console.WriteLine("-----------------------------------------\n");
-                Console.Write("Enter your user name: ");
-                string typedUserName = Console.ReadLine();
-
-                foreach (string checkedUserName in inputUserName)
-                {
-                    if (typedUserName == checkedUserName)
-                    {
-                        Console.Write("Enter your password: ");
-                        string typedPassword = Console.ReadLine();
-
-                        foreach (string checkedPassword in inputPassword)
-                        {
-                            if (typedPassword == checkedPassword)
-                            {
-                                Console.WriteLine("Authentication Successful");
-                                Console.ReadKey();
-                            }
-                            else
-                            {
-                                Console.WriteLine("Invalid Password");
-                                attempsAtLogin++;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid User Name");
-                    }
-
-                }
-            }
-            if (attempsAtLogin > 2)
-                Console.WriteLine("You have failed to authenticate.");
-            else
-            {
-                Console.WriteLine("You have successfully authenticated.");
-            }
-            Console.ReadKey();
-        }
-
-        private static void EstablishUser()
-        {
-            //inputUserName.Add("username");
-            Console.Clear();
-            Console.WriteLine("\nPASSWORD AUTHENTICATION SYSTEM\n");
-            Console.WriteLine("Setup New User Account\n");
-            Console.WriteLine("-----------------------------------------\n");
-            Console.Write("Enter a user name: ");
-            string typedUserName = (Console.ReadLine());
-
-
-            foreach (string checkUserName in inputUserName)
-            {
-                if (checkUserName == typedUserName)
-                {
-                    Console.WriteLine("This User Name is already in use.\n");
-                    Console.WriteLine("Press enter to return.");
-                    Console.ReadKey();
-                }
-                else
-                {
-                    inputUserName.Add(Console.ReadLine());
-                    EstablishPassword();
-                }
-            }
-        }
-
-        //TODO Add encryption of some kind here
-        private static void EstablishPassword()
-        {
-            inputPassword.Add("password");
-            Console.Write("Enter a password: ");
-            inputPassword.Add (Console.ReadLine());
-            Console.WriteLine("Password Saved");
-            Console.ReadKey();
         }
     }
 }
---
+++ Password_Encryption_and_Authorization/Users.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_Encryption_and_Authorization
+{
+    class Users
+    {
+        string username;
+        string password;
+
+        //Public callable method to acquire username and password
+        public void UserPassword()
+        {
+            GetUserNameAndPassword();
+        }
+
+        //Method to read in new username and password
+        void GetUserNameAndPassword()
+        {
+            this.username = Console.ReadLine();
+            this.password = Console.ReadLine();
+        }
+
+        //method to return false if the username and password exist in the list already
+        bool CheckUserNameAndPassword(string username, string password)
+        {
+            return ((this.username == username) && (this.password == md5sum(password)));
+        }
+    }
+}
